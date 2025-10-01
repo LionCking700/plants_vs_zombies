6ea517ebbe352c1a7053773a8976bf712584f4a8
Sarai Sandoval <156473953+SaritaSan@users.noreply.github.com>
2025-09-30 18:47:39 -0600

    monedita brillosa

--- Assets/scripsts/ClickRaycast.cs
+++ Assets/scripsts/ClickRaycast.cs
@@ -10,9 +10,9 @@ public class ClickRaycast : MonoBehaviour
     [SerializeField]
 
     private string coinTag = "Coin";
-     [SerializeField]
+    [SerializeField]
 
-    private UnityEvent onCoinCollected;
+    private UnityEvent<Transform> onCoinCollected;
     [SerializeField]
 
     private bool isActive = true;
@@ -38,7 +38,7 @@ public class ClickRaycast : MonoBehaviour
 
     private void PressCoin(GameObject coin)
     {
-        onCoinCollected.Invoke();
+        onCoinCollected.Invoke(coin.transform);
         coin.GetComponent<Coin>().Collect();
     }
 }
---
+++ Assets/scripsts/LaneManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class LaneManager : MonoBehaviour
+{
+    [SerializeField]
+
+    privata Lane[] lane;
+
+    public Transform GatFrameInLane()
+    {
+        int laneIndex = Random.Range(0, lanes.Length);
+        Lane selactedLae = Random.Range(0, selectedLane.Frames.Count);
+        return selected selectedLane.Frames[frame]
+    }
+}
