6eadbbb54368420cdc7301c304ba1ed29a629f84
Sarai Sandoval <156473953+SaritaSan@users.noreply.github.com>
2025-10-14 18:50:04 -0600

    debe caminar

---
+++ Assets/scripsts/EnemyData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public float attackRange = 5f;
+    public float timeBetweenAttacks = 2f;
+    public float health = 100f;
+    public float speed = 5f;
+    public int damage = 10;
+    public string enemyName = "Zombie";
+    public string attackAnimation = "Attack";
+    public string deathAnimation = "Die";
+    public string walkAnimation = "Walk";
+    public string hitAnimation = "Hit";
+    public string winAnimation = "Win";
+    public string idleAnimation = "Idle";
+}
+
